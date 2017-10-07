aa2cf220820c452d2a4c85e2cf8bfafb860dbc3d
baelarkash <alberchis@gmail.com>
2017-10-07 15:19:33 +0200

    mas Parametros

--- The end of mythology/Assets/Scripts/CharacterParameters.cs
+++ The end of mythology/Assets/Scripts/CharacterParameters.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CharacterParameters : MonoBehaviour {
 
+    public float maxLife;
     public float life;
     public float damage;
     public float speed;
@@ -11,11 +12,17 @@ public class CharacterParameters : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        life = maxLife;
+        damage = 1;
+        speed = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void quitarVida(float damage)
+    {
+        life -= damage;
+    }
 }
