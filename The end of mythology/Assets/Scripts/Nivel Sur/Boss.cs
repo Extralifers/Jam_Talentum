af5587949e3b2412f4ced8059eb836d86d9b77cb
valli22 <davidvacasmiguel@hotmail.com>
2017-10-08 11:29:33 +0200

    Inicio nivel sur
    -Script boss generico
    -Inicio nivel sur

---
+++ The end of mythology/Assets/Scripts/Nivel Sur/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	public float maxLife;
+	public float life;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void takeDamage(float damage){
+		life -= damage;
+	}
+}
